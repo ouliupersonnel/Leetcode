fe8d99d99ea4301b47db7ce19baae7609d113fc4
ouliupersonnel <oliu@finivation.com>
2019-01-22 21:12:08 -0500

    121 Version 2

--- Array/121.cs
+++ Array/121.cs
@@ -1,21 +1,21 @@
-// 112ms, 62.33%, 10m, 2t
+// 108ms, 82.90%, 10m, 2t
 public class Solution {
     public int MaxProfit(int[] prices) {
         if (prices == null || prices.Length == 0)
             return 0;
         
-        int s = 0, e = 0, p = 0;
+        int s = 0, ev = prices[0], p = 0;
         for (int i = 1; i < prices.Length; ++i){
-            if (prices[i] > prices[e]){
-                e = i;
-                p = Math.Max(p, prices[e] - prices[s]);
+            if (prices[i] > ev){
+                ev = prices[i];                
             }
             else if (prices[i] <= prices[s]){
+                p = Math.Max(p, ev - prices[s]);
                 s = i;
-                e = i;
+                ev= prices[i];                
             }                           
         }
         
-        return p;
+        return Math.Max(p, ev - prices[s]);
     }
 }
